49fd672bdbb9d89e7c839fda6c119646537ec86e
balkrishnray <balkrishnaroy31@gmail.com>
2023-06-22 11:26:18 +0545

    web development

---
+++ tut35.js
@@ -0,0 +1,34 @@
+// console.log("Hello This is balkrishna from sagarmatha boys hostel");
+
+function student(name,faculty){
+console.log(name + " is a student of Himalaya College of Engineering\n");
+console.log(name + "'s faculty is " + faculty);
+}
+let name1 = "Aastha", name0 = "Ayush", name2 = "Avishek", name3 = "Ayush";
+let faculty1 = "BCT", faculty0 = "BEX", faculty2 = "BCE", faculty3 = "BAR";
+student(name0,faculty0);
+student(name1,faculty1);
+student(name2,faculty2);
+student(name3,faculty3);
+
+function sum (){
+    let z =  "a" + "b" +"c"; 
+    return z;
+
+}
+
+
+// let o = sum(1,2,3);
+// console.log(o);
+
+// let a=10;
+// let  age = a >= 18?"adult":"teenager";
+// console.log(age);
+
+
+function membership(ismember){
+    return (ismember?"can enter swimming pool":"can't enter swimming pool");
+}
+ 
+console.log(membership(false));
+
